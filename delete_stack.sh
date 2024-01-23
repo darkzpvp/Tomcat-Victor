8a5f4c4b83d50f4937d3d87f8941698301e1c0c8
darkzpvp <victor1val@hotmail.es>
2024-01-23 21:04:02 +0100

    Nuevo

--- delete_stack.sh
+++ delete_stack.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-
+export AWS_ACCESS_KEY_ID=
+export AWS_SECRET_ACCESS_KEY=
+export AWS_SESSION_TOKEN=
+export AWS_DEFAULT_REGION=us-east-1
 # Elimimar pila
 aws cloudformation delete-stack \
 --stack-name "instanciaVictor"  \
